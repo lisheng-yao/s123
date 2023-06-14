bc1be7452db42757761dd9a77c8f43848b7500ac
Leo <leopeter800@gmail.com>
2023-06-14 18:05:53 +0800

    no message

--- src/main/java/com/shop/controller/ShopCarouselServlet.java
+++ src/main/java/com/shop/controller/ShopCarouselServlet.java
@@ -37,109 +37,102 @@ public class ShopCarouselServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		
+
 		PrintWriter out = response.getWriter();
-		
 
 		List<ShopCarouselVO> list = shopCarouselService.getAll();
 
 		out.write(gson.toJson(list));
 
-		
 //	================================== 新增資料 =============================================
-		
+
 		String contentType = request.getContentType();
 		String dataTypeAction = request.getHeader("X-Data-Type");
-        if ("json1".equals(dataTypeAction) && contentType != null && contentType.startsWith("application/json")) {
+		if ("json1".equals(dataTypeAction) && contentType != null && contentType.startsWith("application/json")) {
 //        	String json="";
-            // 讀取請求的 JSON 內容
-            BufferedReader reader = request.getReader();
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            reader.close();
+			// 讀取請求的 JSON 內容
+			BufferedReader reader = request.getReader();
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			reader.close();
 
-            // 解析 JSON 內容
-            JsonObject json = gson.fromJson(sb.toString(), JsonObject.class);
+			// 解析 JSON 內容
+			JsonObject json = gson.fromJson(sb.toString(), JsonObject.class);
 
-            // 從解析後的 JSON 中獲取相應的屬性值
-            String shopCarouselTitle = json.get("shopCarouselTitle").getAsString();
-            String shopCarouselText = json.get("shopCarouselText").getAsString();
-            // 將 Base64 字串轉換為二進位資料
-            String base64Image = json.get("shopCarouselPic").getAsString();
-            // Base64字串中移除資料類型的部分 data:image/jpeg;base64,/!!!
-            base64Image = base64Image.substring(base64Image.indexOf(",") + 1);
-            // System.out.println(base64Image);
-            byte[] shopCarouselPic = Base64.getDecoder().decode(base64Image);
-            Date shopCarouselStartTime = Date.valueOf(json.get("shopCarouselStartTime").getAsString());
-            Date shopCarouselEndTime = Date.valueOf(json.get("shopCarouselEndTime").getAsString());
-            Integer shopCarouselState = json.get("shopCarouselState").getAsInt();
-            String shopCarouselUrl = json.get("shopCarouselUrl").getAsString();
-            
-            ShopCarouselService svc = new ShopCarouselService();
-            svc.addCarousel(shopCarouselTitle,shopCarouselText,shopCarouselPic,shopCarouselStartTime,shopCarouselEndTime
-            		,shopCarouselState,shopCarouselUrl);
-            
-            
-        }
-        
-        // ========================================== 修改 ===================================================
-        
-        if (("json2".equals(dataTypeAction) && contentType != null && contentType.startsWith("application/json"))) {
+			// 從解析後的 JSON 中獲取相應的屬性值
+			String shopCarouselTitle = json.get("shopCarouselTitle").getAsString();
+			String shopCarouselText = json.get("shopCarouselText").getAsString();
+			// 將 Base64 字串轉換為二進位資料
+			String base64Image = json.get("shopCarouselPic").getAsString();
+			// Base64字串中移除資料類型的部分 data:image/jpeg;base64,/!!!
+			base64Image = base64Image.substring(base64Image.indexOf(",") + 1);
+			// System.out.println(base64Image);
+			byte[] shopCarouselPic = Base64.getDecoder().decode(base64Image);
+			Date shopCarouselStartTime = Date.valueOf(json.get("shopCarouselStartTime").getAsString());
+			Date shopCarouselEndTime = Date.valueOf(json.get("shopCarouselEndTime").getAsString());
+			Integer shopCarouselState = json.get("shopCarouselState").getAsInt();
+			String shopCarouselUrl = json.get("shopCarouselUrl").getAsString();
 
-        	BufferedReader reader = request.getReader();
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            reader.close();
+			ShopCarouselService svc = new ShopCarouselService();
+			svc.addCarousel(shopCarouselTitle, shopCarouselText, shopCarouselPic, shopCarouselStartTime,
+					shopCarouselEndTime, shopCarouselState, shopCarouselUrl);
 
-            // 解析 JSON 內容
-            JsonObject json = gson.fromJson(sb.toString(), JsonObject.class);
-            //  System.out.println(json);
+		}
 
-            // 從解析後的 JSON 中獲取相應的屬性值
-            Integer shopCarouselNo = json.get("updateShopCarouselNo").getAsInt();
-            String shopCarouselTitle = json.get("updateShopCarouselTitle").getAsString();
-            String shopCarouselText = json.get("updateShopCarouselText").getAsString();
-            
-            
-            // 將 Base64 字串轉換為二進位資料
-            String base64Image = json.get("updateShopCarouselPic").getAsString();
-          
-            if (base64Image.isEmpty()) {
-                ShopCarouselService svc = new ShopCarouselService();
-                byte[] shopCarouselPic = svc.getOneShopCarousel(shopCarouselNo).getShopCarouselPic();
-            } else {
-                // Base64字串中移除資料類型的部分 data:image/jpeg;base64,/!!!
-                base64Image = base64Image.substring(base64Image.indexOf(",") + 1);
-                byte[] shopCarouselPic = Base64.getDecoder().decode(base64Image);
-            }
+		// ========================================== 修改
+		// ===================================================
 
+		if (("json2".equals(dataTypeAction) && contentType != null && contentType.startsWith("application/json"))) {
 
-            
-            Date shopCarouselStartTime = Date.valueOf(json.get("updateShopCarouselStartTime").getAsString());
-            Date shopCarouselEndTime = Date.valueOf(json.get("updateShopCarouselEndTime").getAsString());
-            Integer shopCarouselState = json.get("updateShopCarouselState").getAsInt();
-            String shopCarouselUrl = json.get("updateShopCarouselUrl").getAsString();
-            
-            ShopCarouselService svc = new ShopCarouselService();
-            svc.updateShopCarousel(shopCarouselNo,shopCarouselTitle,shopCarouselText,shopCarouselPic,shopCarouselStartTime,shopCarouselEndTime
-            		,shopCarouselState,shopCarouselUrl);
-        	
-        	
-        }
-        
+			BufferedReader reader = request.getReader();
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			reader.close();
+
+			// 解析 JSON 內容
+			JsonObject json = gson.fromJson(sb.toString(), JsonObject.class);
+			// System.out.println(json);
+
+			// 從解析後的 JSON 中獲取相應的屬性值
+			Integer shopCarouselNo = json.get("updateShopCarouselNo").getAsInt();
+			String shopCarouselTitle = json.get("updateShopCarouselTitle").getAsString();
+			String shopCarouselText = json.get("updateShopCarouselText").getAsString();
+
+			// 將 Base64 字串轉換為二進位資料
+			String base64Image = json.get("updateShopCarouselPic").getAsString();
+			byte[] shopCarouselPic;
+
+			if (base64Image.isEmpty()) {
+				ShopCarouselService svc = new ShopCarouselService();
+				shopCarouselPic = svc.getOneShopCarousel(shopCarouselNo).getShopCarouselPic();
+			} else {
+				// Base64字串中移除資料類型的部分 data:image/jpeg;base64,/!!!
+				base64Image = base64Image.substring(base64Image.indexOf(",") + 1);
+				shopCarouselPic = Base64.getDecoder().decode(base64Image);
+			}
+
+			Date shopCarouselStartTime = Date.valueOf(json.get("updateShopCarouselStartTime").getAsString());
+			Date shopCarouselEndTime = Date.valueOf(json.get("updateShopCarouselEndTime").getAsString());
+			Integer shopCarouselState = json.get("updateShopCarouselState").getAsInt();
+			String shopCarouselUrl = json.get("updateShopCarouselUrl").getAsString();
+
+			ShopCarouselService svc = new ShopCarouselService();
+			svc.updateShopCarousel(shopCarouselNo, shopCarouselTitle, shopCarouselText, shopCarouselPic,
+					shopCarouselStartTime, shopCarouselEndTime, shopCarouselState, shopCarouselUrl);
+
+		}
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
-		
-        
+
 	}
 }
